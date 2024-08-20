2800e61b345825f0cbf36b460d201df1ee93c7ee
Mario0903 <mariolozano.0903@gmail.com>
2024-08-21 00:56:55 +0200

    print a report

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 def main():
     file_path = "books/frankenstein.txt"
     file_contents = read_file(file_path)
-    number_of_words = words_counter(file_contents)
-    print(number_of_words)
+
+    num_words = words_counter(file_contents)
+    char_count = character_counter(file_contents)
+    char_list = dict_to_listofdicts(char_count)
+
+    print_report(file_path, num_words, char_list)
 
 def read_file(file_path):
-        with open(file_path) as f:
-            file_contents = f.read()
-            return file_contents
+    with open(file_path) as f:
+        file_contents = f.read()
+        return file_contents
 
 def words_counter(file_contents):
     words = file_contents.split()
@@ -15,5 +19,45 @@ def words_counter(file_contents):
     for word in words:
         n += 1
     return n
+
+def character_counter(file_contents):
+    words = file_contents.split()
+    char_count = {}
+
+    for word in words:
+        for char in word:
+            lower_char = char.lower()
+            if lower_char not in char_count:
+                char_count[lower_char] = 1
+            else:
+                char_count[lower_char] += 1
+    
+    return char_count
+
+def dict_to_listofdicts(dict):
+    list = []
+    for key in dict:
+        new_dict = {}
+        new_dict["character"] = key
+        new_dict["num"] = dict[key]
+        list.append(new_dict)
+        list.sort(reverse=True, key=sort_on)
+    
+    return list
+
+def sort_on (dict):
+    return dict["num"]
+
+def print_report(file_path, num_words, list):
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{num_words} words found in the document\n")
+    
+    for dict in list:
+        if dict["character"].isalpha():
+            print(f"The '{dict["character"]}' character was found {dict["num"]} times")
+    
+    print("--- End report ---")
+
+    
 
 main()
